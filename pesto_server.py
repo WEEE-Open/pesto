9c2b8d0ac9170f0e70b9b4bfa5ac99bc6259d96b
Ludovico Pavesi <ludovico.pavesi@gmail.com>
2021-08-11 01:31:31 +0200

    Twisted

--- pesto_server.py
+++ pesto_server.py
@@ -4,29 +4,28 @@ import subprocess
 from dotenv import load_dotenv
 from io import StringIO
 import os
-import socketserver
+from twisted.internet import reactor, protocol
+from twisted.protocols.basic import LineOnlyReceiver
 import threading
 import collections
 
-sockets_list = dict()
-sockets_list_id = 0
-sockets_list_lock = threading.Lock()
+clients = dict()
+clients_lock = threading.Lock()
 
+# TODO: proper queue, where dischi are first piallati then smartclati then whatever
+# TODO: save commands list for those long running commands, to display it
 commands_list = collections.deque()
 commands_list_event = threading.Event()
 commands_list_lock = threading.Lock()
 
 
-class TurbofresaServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
-    pass
-
-
 class CommandRunner(threading.Thread):
-    def __init__(self):
+    def __init__(self, reac: reactor):
         threading.Thread.__init__(self)
         self.thread_name = "CommandRunner"
         self._go = True
         self._sleep = 5.0
+        self._reactor = reac
 
     def run(self):
         while self._go:
@@ -38,7 +37,8 @@ class CommandRunner(threading.Thread):
                     with commands_list_lock:
                         try:
                             command = commands_list.popleft()
-                            exec_command(command[0], command[1], command[2])
+                            # TODO: launch slow commands in yet another thread, so this one is not blocked
+                            self.exec_command(command[0], command[1], command[2])
                         except IndexError:
                             pass
 
@@ -51,80 +51,109 @@ class CommandRunner(threading.Thread):
                     if len(commands_list) <= 0:
                         commands_list_event.clear()
 
+    def exec_command(self, cmd: str, args: str, the_id: int):
+        print(f"[{the_id}] Received command {cmd}{' with args' if len(args) > 0 else ''}")
+        # This part doesn't work
+        if cmd == 'smartctl':
+            param = self.get_smarctl(args)
+            self.send_msg(the_id, f"smartctl {param}")
+        elif cmd == 'get_disks':
+            param = self.get_disks()
+            self.send_msg(the_id, f"get_disks {param}")
+        elif cmd == 'get_disks_win':
+            param = get_disks_win()
+            self.send_msg(the_id, f"get_disks_win {param}")
+        elif cmd == 'ping':
+            self.send_msg(the_id, f"pong")
+        else:
+            param = json.dumps({"message": "Unrecognized command", "command": cmd}, separators=(',', ':'), indent=None)
+            self.send_msg(the_id, f"error {param}")
+
+    def get_disks(self):
+        # Name is required, otherwise the tree is flattened
+        output = subprocess.getoutput("lsblk -o NAME,PATH,VENDOR,MODEL,SERIAL,HOTPLUG,ROTA,MOUNTPOINT -J")
+        jsonized = json.loads(output)
+        if "blockdevices" in jsonized:
+            result = jsonized["blockdevices"]
+        else:
+            result = []
+        for el in result:
+            mounts = self.find_mounts(el)
+            if "children" in el:
+                del el["children"]
+            if "name" in el:
+                del el["name"]
+            el["mountpoint"] = mounts
+
+        return json.dumps(result, separators=(',', ':'), indent=None)
+
+    @staticmethod
+    def get_smarctl(args):
+        exitcode, output = subprocess.getstatusoutput("sudo smartctl -a " + args)
+        jsonize = {
+            "disk": args,
+            "exitcode": exitcode,
+            "output": output,
+        }
+        return json.dumps(jsonize, separators=(',', ':'), indent=None)
+
+    def find_mounts(self, el: dict):
+        mounts = []
+        if el["mountpoint"] is not None:
+            mounts.append(el["mountpoint"])
+        if "children" in el:
+            children = el["children"]
+            for child in children:
+                mounts += self.find_mounts(child)
+        return mounts
+
+    def send_msg(self, client_id: int, msg: str):
+        with clients_lock:
+            thread = clients.get(client_id)
+            if thread is None:
+                print(f"[{client_id}] Connection already closed while trying to send a message")
+            else:
+                thread: TurboHandler
+                self._reactor.callFromThread(TurboHandler.send_msg, thread, msg)
+
     def stop_asap(self):
         self._go = False
         commands_list_event.set()
         self.join(self._sleep * 2)
 
 
-class TurboHandler(socketserver.StreamRequestHandler):
-    def handle(self):
-        conn_id = self.create_id()
+class TurboHandler(LineOnlyReceiver):
+    def __init__(self):
+        self._id = -1
 
-        client_addr = self.client_address
-        print(f'Connected to {client_addr}, id {conn_id}')
-        while True:
-            data = self.rfile.readline()
-            if not data:
-                # Connection closed
-                break
-
-            # Some command
-            command = data.decode('utf-8').strip()
-
-            # Exit command
-            if command.lower() == 'exit':
-                break
-
-            self.enqueue_command(command, conn_id)
-
-        with sockets_list_lock:
-            del sockets_list[conn_id]
-        # print(f'Closed connection to {client_addr}')
-
-    def send_message(self, text: str):
-        self.wfile.write(f"{text}\n".encode('utf-8'))
-
-    def create_id(self):
-        with sockets_list_lock:
-            global sockets_list_id
-
-            while sockets_list_id in sockets_list:
-                sockets_list_id += 1
-
-            the_chosen_one = sockets_list_id
-            sockets_list[the_chosen_one] = self
-            # Do not reuse it
-            sockets_list_id += 1
-        return the_chosen_one
-
-    @staticmethod
-    def enqueue_command(cmd: str, conn_id: int):
-        parts = cmd.split(' ', 1)
-        cmd = parts[0].lower()
-        args = parts[1] if 1 in parts else ''
+    def connectionMade(self):
+        self._id = self.factory.conn_id
+        self.factory.conn_id += 1
         with commands_list_lock:
-            commands_list.append((cmd, args, conn_id))
-            commands_list_event.set()
+            with clients_lock:
+                clients[self._id] = self
+        print(f"[{str(self._id)}] Client connected")
 
+    def connectionLost(self, reason=protocol.connectionDone):
+        print(f"[{str(self._id)}] Client disconnected")
+        with commands_list_lock:
+            with clients_lock:
+                del clients[self._id]
 
-def exec_command(cmd: str, args: str, id: int):
-    # This part doesn't work
-    if cmd == 'smartctl':
-        output = subprocess.getoutput("smartctl -a " + args)
-        # return output
-    elif cmd == 'get_disks':
-        output = subprocess.getoutput("lsblk -d")
-        disks = get_disks(output)
-        # return disks
-    elif cmd == 'get_disks_win':
-        pass
-        # return get_disks_win()
-    elif cmd == 'ping':
-        self.send_message(f"pong")
-    else:
-        param = json.dumps({"message": "Unrecognized command"}, separators=(',', ':'), indent=None)
-        self.send_message(f"error {param}")
+    def lineReceived(self, line):
+        line = line.decode('utf-8').strip()
+        if line.startswith('exit'):
+            self.transport.loseConnection()
+        else:
+            parts = line.split(' ', 1)
+            cmd = parts[0].lower()
+            args = parts[1] if len(parts) > 1 else ''
+            with commands_list_lock:
+                commands_list.append((cmd, args, self._id))
+                commands_list_event.set()
+
+    def send_msg(self, response: str):
+        self.sendLine(response.encode('utf-8'))
 
 
 def main():
@@ -132,19 +161,27 @@ def main():
     ip = os.getenv("IP")
     port = os.getenv("PORT")
 
-    ch = CommandRunner()
-    ch.start()
+    ch = None
 
     try:
-        with TurbofresaServer((ip, int(port)), TurboHandler) as server:
-            print(f"Listening on {ip} port {port}")
-            server.allow_reuse_address = True
-            server.serve_forever()
+        factory = protocol.ServerFactory()
+        factory.protocol = TurboHandler
+        factory.conn_id = 0
+
+        print(f"Listening on {ip} port {port}")
+        # noinspection PyUnresolvedReferences
+        reactor.listenTCP(int(port), factory, interface=ip)
+
+        ch = CommandRunner(reactor)
+        ch.start()
+
+        # noinspection PyUnresolvedReferences
+        reactor.run()
     except KeyboardInterrupt:
         print("KeyboardInterrupt, terminating")
-        server.shutdown()
     finally:
-        ch.stop_asap()
+        if ch is not None:
+            ch.stop_asap()
 
 
 def load_settings():
@@ -155,16 +192,6 @@ def load_settings():
     # Defaults
     config = StringIO("IP=127.0.0.1\nPORT=1030")
     load_dotenv(stream=config)
-
-
-def get_disks(lsblk):
-    result = []
-    for line in lsblk:
-        if line[0] == 's':
-            temp = " ".join(line.split())
-            temp = temp.split(" ")
-            result.append([temp[0], temp[3]])
-    return str(result)
 
 
 def get_disks_win():
@@ -179,7 +206,7 @@ def get_disks_win():
             size.append(line)
     for idx, line in enumerate(size):
         drive += [[label[idx], line]]
-    return str(drive)
+    return json.dumps(drive, separators=(',', ':'), indent=None)
 
 
 if __name__ == '__main__':
