82f4f76b657686e68f34436f858605f8805d1f8c
Lint Action <lint-action@samuelmeuli.com>
2021-11-26 19:51:00 +0000

    Fix code style issues with Black

--- smartctl_test.py
+++ smartctl_test.py
@@ -4,15 +4,15 @@ import sys
 import utilites
 
 if __name__ == "__main__":
-	if len(sys.argv) != 2:
-		print("Wrong arguments")
-		exit(1)
+    if len(sys.argv) != 2:
+        print("Wrong arguments")
+        exit(1)
 
-	with open(sys.argv[1], 'r') as f:
-		try:
-			res = utilites.smartctl_get_status(utilites.parse_smartctl_output(f.read()))
-			print(res)
-			exit(0)
-		except RuntimeError as e:
-			print("RuntimeError: " + str(e))
-			exit(2)
+    with open(sys.argv[1], "r") as f:
+        try:
+            res = utilites.smartctl_get_status(utilites.parse_smartctl_output(f.read()))
+            print(res)
+            exit(0)
+        except RuntimeError as e:
+            print("RuntimeError: " + str(e))
+            exit(2)
