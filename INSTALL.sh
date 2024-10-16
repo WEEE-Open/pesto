b5ee2686a1ee892c9e321ada60507d17495600a1
Al3cr1s <alexgalea04@gmail.com>
2024-10-16 17:36:48 +0200

    Fix install script

--- INSTALL.sh
+++ INSTALL.sh
@@ -17,9 +17,9 @@ echo "This script will install PESTO on your system, would you like to proceed? 
 echo "                                                                               "
 tput sgr0
 
-read CONFIRM
+read -r CONFIRM
 
-if [[ "$CONFIRM" != 'y' && "$CONFIRM" != 'Y' && "$CONFIRM" = "" ]]; then
+if [[ "$CONFIRM" != 'y' && "$CONFIRM" != 'Y' && "$CONFIRM" != "" ]]; then
   tput setab $RED
   tput setaf $BLACK
   echo "                                     "
@@ -31,20 +31,20 @@ fi
 
 echo "Moving files in working directories ..."
 
-sudo cp -r $CURRENT_DIR /opt/
+sudo cp -r "$CURRENT_DIR" /opt/
 
-sudo cp /opt/$FOLDER_NAME/desktop_files/basilico.desktop /usr/share/applications/
-sudo cp /opt/$FOLDER_NAME/desktop_files/pinolo.desktop /usr/share/applications/
-sudo cp /opt/$FOLDER_NAME/desktop_files/uninstall_pinolo.desktop /usr/share/applications/
-sudo cp /opt/$FOLDER_NAME/basilico.service /etc/systemd/system/
+sudo cp "/opt/$FOLDER_NAME/desktop_files/basilico.desktop" /usr/share/applications/
+sudo cp "/opt/$FOLDER_NAME/desktop_files/pinolo.desktop" /usr/share/applications/
+sudo cp "/opt/$FOLDER_NAME/desktop_files/uninstall_pinolo.desktop" /usr/share/applications/
+sudo cp "/opt/$FOLDER_NAME/basilico.service" /etc/systemd/system/
 
 echo "Generating python virtual environment ..."
 
 PYTHON_VERSION=$(python3 -c 'import sys; print(sys.version_info[1])')
 if [[ "$PYTHON_VERSION" -ge 10 ]]; then
-  sudo python3 -m venv /opt/$FOLDER_NAME/venv
+  sudo python3 -m venv "/opt/$FOLDER_NAME/venv"
 elif [[ "$(which python3.10)" != "" ]]; then
-  sudo python3.10 -m venv /opt/$FOLDER_NAME/venv
+  sudo python3.10 -m venv "/opt/$FOLDER_NAME/venv"
 else
   tput setab $RED
   tput setaf $BLACK
@@ -55,18 +55,19 @@ else
   tput sgr0
   exit 1
 fi
-sudo chown -R $USERNAME:$USERNAME /opt/pesto
-source /opt/$FOLDER_NAME/venv/bin/activate
-pip install -r /opt/$FOLDER_NAME/requirements_client.txt
-pip install -r /opt/$FOLDER_NAME/requirements_server.txt
+
+sudo chown -R "$USERNAME:$USERNAME" "/opt/$FOLDER_NAME"
+source "/opt/$FOLDER_NAME/venv/bin/activate"
+pip install -r "/opt/$FOLDER_NAME/requirements_client.txt"
+pip install -r "/opt/$FOLDER_NAME/requirements_server.txt"
 deactivate
 
 echo "Installing system dependencies"
-if [[ "$(where apt)" != "" ]]; then
+if [[ "$(which apt)" != "" ]]; then
   sudo apt update
-  sudo apt install cloud-utils smartmontools
-elif [["$(where pacman)" != "" ]]; then
-  sudo pacman -Sy cloud-utils smartmontools
+  sudo apt install -y cloud-utils smartmontools
+elif [[ "$(which pacman)" != "" ]]; then
+  sudo pacman -Sy --noconfirm cloud-utils smartmontools
 else
   tput setab $RED
   tput setaf $BLACK
@@ -82,6 +83,6 @@ sudo systemctl daemon-reload
 tput setaf $BLACK
 tput setab $GREEN
 echo "                                                                                      "
-echo "Installation completed succsessfully! Search for PINOLO in your applications launcher."
+echo "Installation completed successfully! Search for PINOLO in your applications launcher."
 echo "                                                                                      "
 tput sgr0
