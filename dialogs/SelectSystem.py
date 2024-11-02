e25b470ac5f87c80f462344a29e3e40b69327409
il-palmi <il-palmi@users.noreply.github.com>
2024-11-03 00:07:59 +0100

    Settings dialog

--- dialogs/SelectSystem.py
+++ dialogs/SelectSystem.py
@@ -1,25 +1,18 @@
+import os.path
+
 from PyQt5.QtCore import QObject
 from PyQt5.QtWidgets import QDialog
 
 from ui.SelectSystemDialog import Ui_SelectSystemDialog
-from typing import TYPE_CHECKING
-
-if TYPE_CHECKING:
-    from pinolo import PinoloMainWindow
-    from dialogs.NetworkSettings import NetworkSettings
 
 
 class SelectSystemDialog(QDialog, Ui_SelectSystemDialog):
-    def __init__(self, parent: QObject, is_load_system: bool, path: str, images: list = None, network_settings_dialog: "NetworkSettings" = None):
+    def __init__(self, parent: QObject):
         super(SelectSystemDialog, self).__init__(parent)
         self.setupUi(self)
 
-        self.is_load_system = is_load_system
-        self.path = path
-        self.images = images
-        self.network_settings_dialog = network_settings_dialog
-
-        self.files = []
+        self.image_paths = []
+        self.selected_image = None
 
         self.setup()
         self.show()
@@ -28,16 +21,14 @@ class SelectSystemDialog(QDialog, Ui_SelectSystemDialog):
         self.selectButton.clicked.connect(self.select)
         self.cancelButton.clicked.connect(self.close)
 
-    def ask_for_image(self, images: list):
-        self.images = images
-        for img in images:
-            img = img.rsplit("/", 1)[1]
-            img = img.rsplit(".")
-            if len(img) > 1:
-                if img[1] == "iso" or img[1] == "img":
-                    self.files.append(f"{img[0]}.{img[1]}")
-        self.isoList.addItems(self.files)
-        self.show()
+    def load_images(self, images: list):
+        for path in images:
+            if not os.path.isfile(path):
+                continue
+
+            file_extension = os.path.splitext(path)[1]
+            if file_extension == ".iso" or file_extension == ".img":
+                self.isoList.addItem(os.path.basename(path))
 
     def select(self):
         """
@@ -45,19 +36,10 @@ class SelectSystemDialog(QDialog, Ui_SelectSystemDialog):
         """
 
         if self.isoList.currentItem() is None:
-            print("GUI: No image selected.")
             return
 
-        iso = self.isoList.currentItem().text()
-        for iso_dir in self.images:
-            if iso in iso_dir:
-                if self.is_load_system:
-                    pinolo: "PinoloMainWindow" = self.parent()
-                    pinolo.load_selected_system(iso_dir, iso)
-                    break
-                else:
-                    network_settings: "NetworkSettings" = self.parent()
-                    network_settings.defaultSystemLineEdit.setText(iso_dir)
-                    break
+        self.selected_image = self.isoList.currentItem().text()
+        self.accept()
 
-        self.close()
+    def get_selected_image(self):
+        return self.selected_image
